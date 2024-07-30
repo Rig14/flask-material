cca33bb608a70174dab5bf7de45af6fa6d405092
ririvi <ririvi@taltech.ee>
2024-07-30 18:27:04 +0300

    visual

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from openweather import weather_data
+import reverse_geocode
 
 app = Flask(__name__)
 app.config.from_object('config:Config')
@@ -15,8 +16,9 @@ def weather():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
 
+    location = reverse_geocode.get((lat, lon))
     data = weather_data(lat, lon, app.config['OPEN_WEATHER_API_KEY'])
-    return render_template('weather.html', data=data)
+    return render_template('weather.html', data=data, location=location)
 
 if __name__ == '__main__':
     app.run()
