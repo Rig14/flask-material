c86e69e290af0d08769f48b4f2a73b4f750fe4f5
ririvi <ririvi@taltech.ee>
2024-08-04 21:29:32 +0300

    db

--- config.example.py
+++ config.example.py
@@ -3,3 +3,9 @@
 class Config:
     # Enter your OpenWeather API key here
     OPEN_WEATHER_API_KEY = ""
+    POSTGRES_DATABASE_NAME = ""
+    POSTGRES_USER = ""
+    POSTGRES_PASSWORD = ""
+    POSTGRES_HOST = ""
+    POSTGRES_POST = ""
+
