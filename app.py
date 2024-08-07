7d4d41b5affac953c97f374b148d5d3c26cc3896
rasand <rasand@taltech.ee>
2024-08-07 23:26:59 +0300

    Added login and logout (without hashing) & flash messages

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ import reverse_geocode
 from flask import Flask, redirect
 from flask import render_template
 from flask import request
+from flask import session, flash
 
 from database.database import create_database
-from database.user import create_user
+from database.user import create_user, check_user
 from openweather import weather_data
 
 app = Flask(__name__)
 app.config.from_object('config:Config')
+app.secret_key = 'your_secret_key'
 with app.app_context():
     create_database()
-
 
 @app.route('/')
 def home():
@@ -33,6 +34,35 @@ def signup():
     name = request.form.get('name')
     password = request.form.get('password')
     create_user(name, password)
+    user_id = check_user(name, password)
+    if user_id:
+        session['user_id'] = user_id
+        session['username'] = name
+        flash("User created")
+        return redirect("/")
+    flash("Couldn't create user")
+    return redirect("/")
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    name = request.form.get('name')
+    password = request.form.get('password')
+    user_id = check_user(name, password)
+    if user_id:
+        session['user_id'] = user_id
+        session['username'] = name
+        flash("Login successful")
+        return redirect("/")
+    flash('Invalid credentials')
+    return redirect("/")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('username', None)
+    flash('Logged out')
     return redirect("/")
 
 
